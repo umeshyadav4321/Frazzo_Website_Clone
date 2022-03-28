051ce81470796c5a259fab4ae61a545d60bde91f
umeshyadav4321 <80197485+umeshyadav4321@users.noreply.github.com>
2022-03-28 11:06:03 +0530

    navbar/footer add

---
+++ index.js
@@ -0,0 +1,30 @@
+import { navbar } from "./components/navbar.js";
+document.getElementById("Navbar").innerHTML=navbar();
+
+
+import { footer } from "./components/footer.js";
+document.getElementById("Footer-Cont").innerHTML=footer();
+
+
+var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+
