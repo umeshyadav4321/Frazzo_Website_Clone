fa15011cc510db44bef64700cee2ec909aa7ce97
umeshyadav4321 <80197485+umeshyadav4321@users.noreply.github.com>
2022-03-29 11:15:58 +0530

    ....

--- products.js
+++ products.js
@@ -1,62 +1,20 @@
+var produs=JSON.parse(localStorage.getItem("prods"));
+// console.log(produs)
 
- var products=JSON.parse(localStorage.getItem("Prods"))||[];
-
- console.log(displayProducts(products));
-
-//  console.log(1)
- 
-
-//  display(products)
-//  function pricesort(){
-//    var select=document.querySelector("#sort").value;
-//    if(select=="low"){
-//      products.sort(function(a,b){
-//        return Number(a.price)-Number(b.price);
-       
-//      })
-//      console.log(products);
-//    }
-//   if(select=="high"){
-//      products.sort(function(a,b){
-//        return Number(b.price)-Number(a.price);
-       
-//      })
-//      console.log(products);
-//    }
-
-//    display(products);
-//  }
-
-// //  CODE FOR THE FILTER
-// function filter(){
-// selected=document.querySelector("#category").value;
-// filterList=products.filter(function(elem){
-//        if (elem.type==selected){
-//          return elem.type
-//        }
-
-// }) 
-
-// display(filterList);
-// if(selected=="shopall"){
-//    display(products);
-// }
-
-// }
-
-function displayProducts(products){
-    products.forEach(function(el){
+displayProducts(produs);  
+function displayProducts(produs){
+    produs.forEach(function(el){
         var div=document.createElement("div");
         var image=document.createElement("img");
-        image.setAttribute("src","el.image");
+        image.setAttribute("src",el.image);
         var name=document.createElement("p");
         name.innerHTML=el.name;
         var qty=document.createElement("p");
         qty.innerHTML=el.Qty;
-        var price=document.createElement("p");
-        price.innerHTML=el.price;
-        div.append(image,name,qty,price);
-        document.getElementById(main).append(div);
+        var prices=document.createElement("p");
+        prices.innerHTML=el.price;
+        div.append(image,name,qty,prices);
+        document.getElementById("main").append(div);
     });
 };
 
