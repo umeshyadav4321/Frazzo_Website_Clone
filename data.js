f9282cacfa2a25f4c9315e58dcb3dfe4b1eb5563
umeshyadav4321 <80197485+umeshyadav4321@users.noreply.github.com>
2022-03-29 19:07:19 +0530

    product page

--- data.js
+++ data.js
@@ -1,77 +1,89 @@
 var data=[
     {
-        "image":"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/135/original/data?1612689931",
-        "name":"Mango Badami",
-        "Qty":"1kg",
-        "price":"₹199"
+        image:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/135/original/data?1612689931",
+        name:"Mango Badami",
+        Qty:"1kg",
+        price:199,
+        type:"mangos",
+
 
     },
     {
-        "image":"https://s3.ap-south-1.amazonaws.com/fraazo-prod/images/images/000/004/597/original/data?1647577454",
-        "name":"Mango Totapuri",
-        "Qty":"1kg",
-        "price":"₹129"
+        image:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/images/images/000/004/597/original/data?1647577454",
+        name:"Mango Totapuri",
+        Qty:"1kg",
+        price:129,
+        type:"mangos",
 
     },
     {
-        "image":"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/250/original/data?1621065378",
-        "name":"Mango Lalbhag Sindhura",
-        "Qty":"1kg",
-        "price":"₹199"
+        image:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/250/original/data?1621065378",
+        name:"Mango Lalbhag Sindhura",
+        Qty:"1kg",
+        price:199,
+        type:"mangos",
 
     },
     {
-        "image":"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VMAN12.png",
-        "name":"Mango Raw",
-        "Qty":"250g",
-        "price":"₹29"
+        image:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VMAN12.png",
+        name:"Mango Raw",
+        Qty:"250g",
+        price:29,
+        type:"mangos",
 
     },
     {
-        "image":"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FBAN14.png",
-        "name":"Banana Robusta",
-        "Qty":"500g",
-        "price":"₹19"
+        image:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FBAN14.png",
+        name:"Banana Robusta",
+        Qty:"500g",
+        price:19,
+        type:"fruits",
 
     },
     {
-        "image":"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FBAN24.png",
-        "name":"Banana Elaichi/Yellaki",
-        "Qty":"250g",
-        "price":"₹9"
+        image:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FBAN24.png",
+        name:"Banana Elaichi/Yellaki",
+        Qty:"250g",
+        price:9,
+        type:"fruits",
 
     },
     {
-        "image":"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FCHI13.png",
-        "name":"Chikoo",
-        "Qty":"500g",
-        "price":"₹19"
+        image:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FCHI13.png",
+        name:"Chikoo",
+        Qty:"500g",
+        price:19,
+        type:"fruits",
 
     },
     {
-        "image":"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FGRA42.png",
-        "name":"Grapes",
-        "Qty":"500",
-        "price":"₹59"
+        image:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FGRA42.png",
+        name:"Grapes",
+        Qty:"500",
+        price:59,
+        type:"fruits",
+        type:"fruits",
 
     },
     {
-        "image":"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/247/original/data?1612371028",
-        "name":"Banana Nendran",
-        "Qty":"500g",
-        "price":"₹66"
+        image:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/247/original/data?1612371028",
+        name:"Banana Nendran",
+        Qty:"500g",
+        price:66,
+        type:"fruits",
 
     },
     {
-        "image":"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/260/original/data?1612450264",
-        "name":"Banana Raw",
-        "Qty":"500",
-        "price":"₹21"
+        image:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/260/original/data?1612450264",
+        name:"Raw Banana",
+        Qty:"500",
+        price:21,
+        type:"fruits",
 
-    }
-];
+    },
+]
 
-var products=localStorage.setItem("prods",JSON.stringify(data));
+localStorage.setItem("prods",JSON.stringify(data));
 // console.log(5);
 // var prods=JSON.parse(localStorage.getItem("Prod"))||[];
 // data.map(function(elem){
--- products.js
+++ products.js
@@ -1,20 +1,67 @@
-var produs=JSON.parse(localStorage.getItem("prods"));
+var produs=JSON.parse(localStorage.getItem("prods"))||[];
 // console.log(produs)
+// function handlepriceSort(){
+//     var selected=document.querySelector("#pricesort").value;
+//     console.log(selected);
+//     if(selected=="high"){
+//       produs.sort(function(a,b){
+//           return Number(b.price)-Number(a.price);
+//       })
+//       console.log(produs)
 
-displayProducts(produs);  
+//     }
+//     if(selected=="low"){
+//         produs.sort(function(a,b){
+//             return Number(a.price)-Number(b.price);
+//         });
+//         console.log(produs)
+
+//     }
+   
+  
+// }
+// displayProducts(produs)
+
+// displayProducts(produs);
+displayProducts(produs);
+function filterbycat(){
+    // displayProducts.innerHTML=" ";
+    var selected=document.querySelector("#category").value;
+    console.log(selected);
+
+    var filteredList=produs.filter(function(elem){
+        return elem.type==selected;
+    })
+    displayProducts(filteredList);
+    if(selected=="shopall"){
+        displayProducts(produs)
+    }
+}
 function displayProducts(produs){
+    document.querySelector("#main").innerHTML="";
     produs.forEach(function(el){
         var div=document.createElement("div");
+        div.setAttribute("id","MainDiv");
         var image=document.createElement("img");
         image.setAttribute("src",el.image);
         var name=document.createElement("p");
         name.innerHTML=el.name;
+    //    var divone=document.createElement("div")
         var qty=document.createElement("p");
         qty.innerHTML=el.Qty;
         var prices=document.createElement("p");
         prices.innerHTML=el.price;
-        div.append(image,name,qty,prices);
+        var but=document.createElement("button");
+        but.setAttribute("id","buttonP")
+        but.textContent="Add";
+        div.append(image,name,qty,prices,but);
         document.getElementById("main").append(div);
     });
 };
+
+// import { navbar } from "./components/navbar.js";
+// document.querySelector("#navbar-container").innerHTML= navbar();
+
+// import {footer} from "./components/footer.js";
+// document.querySelector("#footer").innerHTML=footer();
 
